03c1015037a57acb1833c902c3559655455245d8
Romirh47 <217002020@student.unsil.ac.id>
2024-06-14 19:15:50 +0700

    buat laravel dan frontend

---
+++ resources/views/index.blade.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Matdash Free</title>
+  <link rel="shortcut icon" type="image/png" href="../assets/images/logos/favicon.png" />
+  <link rel="stylesheet" href="../assets/css/styles.min.css" />
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+    data-sidebar-position="fixed" data-header-position="fixed">
+    <!-- Sidebar Start -->
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="./index.html" class="text-nowrap logo-img">
+            <img src="../assets/images/logos/logo.svg" alt="" />
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-8"></i>
+          </div>
+        </div>
+        <!-- Sidebar navigation-->
+       @include('sidebar')
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!--  Sidebar End -->
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+      <!--  Header Start -->
+      <header class="app-header">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <ul class="navbar-nav">
+            <li class="nav-item d-block d-xl-none">
+              <a class="nav-link sidebartoggler " id="headerCollapse" href="javascript:void(0)">
+                <i class="ti ti-menu-2"></i>
+              </a>
+            </li>
+          </ul>
+          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
+            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+              <a href="https://adminmart.com/product/matdash-free-bootstrap-5-admin-dashboard-template/" target="_blank" class="btn btn-primary">hello {{ Auth::user()->name }}</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link " href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  <img src="../assets/images/profile/user-1.jpg" alt="" width="35" height="35" class="rounded-circle">
+                </a>
+                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
+                  <div class="message-body">
+                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                      <i class="ti ti-user fs-6"></i>
+                      <p class="mb-0 fs-3">My Profile</p>
+                    </a>
+                    <a href="{{route('logout')}}" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <!--  Header End -->
+      <div class="body-wrapper-inner">
+        <div class="container-fluid">
+          <!--  Row 1 -->
+            <div class="col-lg-4">
+              <div class="card overflow-hidden hover-img">
+                <div class="position-relative">
+                  <a href="javascript:void(0)">
+                    <img src="../assets/images/blog/blog-img3.jpg" class="card-img-top" alt="matdash-img">
+                  </a>
+                  <span
+                    class="badge text-bg-light text-dark fs-2 lh-sm mb-9 me-9 py-1 px-2 fw-semibold position-absolute bottom-0 end-0">2
+                    min Read</span>
+                  <img src="../assets/images/profile/user-3.jpg" alt="matdash-img"
+                    class="img-fluid rounded-circle position-absolute bottom-0 start-0 mb-n9 ms-9" width="40"
+                    height="40" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Georgeanna Ramero">
+                </div>
+                <div class="card-body p-4">
+                  <span class="badge text-bg-light fs-2 py-1 px-2 lh-sm  mt-3">Health</span>
+                  <a class="d-block my-4 fs-5 text-dark fw-semibold link-primary" href="">bacottttt goblokk
+                    lockdowns
+                    loom in China</a>
+                  <div class="d-flex align-items-center gap-4">
+                    <div class="d-flex align-items-center gap-2">
+                      <i class="ti ti-eye text-dark fs-5"></i>9,480
+                    </div>
+                    <div class="d-flex align-items-center gap-2">
+                      <i class="ti ti-message-2 text-dark fs-5"></i>12
+                    </div>
+                    <div class="d-flex align-items-center fs-2 ms-auto">
+                      <i class="ti ti-point text-dark"></i>Sat, Dec 17
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/js/sidebarmenu.js"></script>
+  <script src="../assets/js/app.min.js"></script>
+  <script src="../assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src="../assets/libs/simplebar/dist/simplebar.js"></script>
+  <script src="../assets/js/dashboard.js"></script>
+  <!-- solar icons -->
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,103 @@
+<!-- Body Wrapper -->
+<div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+ data-sidebar-position="fixed" data-header-position="fixed">
+
+ <!-- Sidebar Start -->
+ <aside class="left-sidebar">
+   <!-- Sidebar scroll -->
+   <div>
+     <!-- Brand Logo -->
+     <div class="brand-logo d-flex align-items-center justify-content-between">
+       <a href="./index.html" class="text-nowrap logo-img">
+         <img src="../assets/images/logos/logo.svg" alt="" />
+       </a>
+       <!-- Close Button for Mobile View -->
+       <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+         <i class="ti ti-x fs-8"></i>
+       </div>
+     </div>
+
+     <!-- Sidebar navigation -->
+     <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+       <ul id="sidebarnav">
+         <!-- Home Section -->
+         <li class="nav-small-cap">
+           <iconify-icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></iconify-icon>
+           <span class="hide-menu">Home</span>
+         </li>
+         <li class="sidebar-item">
+           <a class="sidebar-link" href="./index.html" aria-expanded="false">
+             <iconify-icon icon="solar:widget-add-line-duotone"></iconify-icon>
+             <span class="hide-menu">Dashboard</span>
+           </a>
+         </li>
+         <!-- Divider -->
+         <li>
+           <span class="sidebar-divider lg"></span>
+         </li>
+         <!-- Sensors Section -->
+         <li class="nav-small-cap">
+           <iconify-icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></iconify-icon>
+           <span class="hide-menu">SENSORS</span>
+         </li>
+         <li class="sidebar-item">
+           <a class="sidebar-link" href="./ui-buttons.html" aria-expanded="false">
+             <iconify-icon icon="solar:layers-minimalistic-bold-duotone"></iconify-icon>
+             <span class="hide-menu">TEMPERATURE</span>
+           </a>
+         </li>
+         <li class="sidebar-item">
+           <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
+             <iconify-icon icon="solar:danger-circle-line-duotone"></iconify-icon>
+             <span class="hide-menu">HUMIDITY</span>
+           </a>
+         </li>
+         <li class="sidebar-item">
+           <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
+             <iconify-icon icon="solar:bookmark-square-minimalistic-line-duotone"></iconify-icon>
+             <span class="hide-menu">INTENSITY</span>
+           </a>
+         </li>
+         <li class="sidebar-item">
+           <a class="sidebar-link" href="./ui-forms.html" aria-expanded="false">
+             <iconify-icon icon="solar:file-text-line-duotone"></iconify-icon>
+             <span class="hide-menu">MOISTURES</span>
+           </a>
+         </li>
+         <!-- Divider -->
+         <li>
+           <span class="sidebar-divider lg"></span>
+         </li>
+         <!-- Actuator Section -->
+         <li class="nav-small-cap">
+           <iconify-icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></iconify-icon>
+           <span class="hide-menu">AKTUATOR</span>
+         </li>
+         <li class="sidebar-item">
+           <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
+             <iconify-icon icon="solar:login-3-line-duotone"></iconify-icon>
+             <span class="hide-menu">AKTUATOR</span>
+           </a>
+         </li>
+         <!-- Divider -->
+         <li>
+           <span class="sidebar-divider lg"></span>
+         </li>
+         <!-- Extra Section -->
+         <li class="nav-small-cap">
+           <iconify-icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></iconify-icon>
+           <span class="hide-menu">EXTRA</span>
+         </li>
+         <li class="sidebar-item">
+            <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
+              <iconify-icon icon="solar:user-plus-rounded-line-duotone"></iconify-icon>
+              <span class="hide-menu">USERS</span>
+            </a>
+          </li>
+       </ul>
+     <!-- End Sidebar navigation -->
+   </div>
+   <!-- End Sidebar scroll -->
+ </aside>
+ <!-- Sidebar End -->
+</div>
